407b793f9416fd57f4f5931e5b37b5ffb56c472f
Bhaskar Ghosh <bhaskar4647@gmail.com>
2023-09-05 15:53:31 -0500

    Visual transformer coded

--- VisTrans.py
+++ VisTrans.py
@@ -211,3 +211,69 @@ class Block(nn.Module):
         x = x + self.mlp(self.norm2(x))
 
         return x
+    
+class Transformer(nn.Module):
+    def __init__(
+            self,
+            img_size=384,
+            patch_size=16,
+            in_chans=3,
+            n_classes=1000,
+            embed_dim=768,
+            depth=12,
+            n_heads=12,
+            mlp_ratio=4.,
+            qkv_bias=True,
+            p=0.,
+            attn_p=0.,
+    ):
+        super().__init__()
+        self.patch_embed = PatchEmbed(
+            img_size=img_size,
+            patch_size=patch_size,
+            in_chans=in_chans,
+            embed_dim=embed_dim,
+        )
+        self.cls_token = nn.Parameter(torch.zeros(1,1,embed_dim))
+        self.pos_embed = nn.Parameter(
+            torch.zeros(1,1+self.patch_embed.n_patches, embed_dim)
+        )
+        self.pos_drop = nn.Dropout(p=p)
+        self.blocks = nn.ModuleList(
+            [
+                Block(
+                    dim = embed_dim,
+                    n_heads=n_heads,
+                    mlp_ratio = mlp_ration,
+                    qkv_bias=qkv_bias,
+                    p=p,
+                    attn_p=attn_p,
+                )
+                for _ in range(depth)
+            ]
+        )
+
+        self.norm = nn.LayerNorm(embed_dim, eps=1e-6)
+        self.head = nn.Linear(embed_dim, n_classes)
+
+
+    def forward(self,x):
+        n_samples = x.shape[0]
+        x = self.patch_embed(x)
+
+        cls_token = self.cls_token.expand(
+            n_samples, -1, -1
+        )
+        x = torch.cat((cls_token, x), dim=1)
+        x = x + self.pos_embed
+        x = self.pos_drop
+
+        for block in self.blocks:
+            x = block(x)
+
+        x = self.norm(x)
+
+        cls_token_final = x[:,0]
+        x = self.head(cls_token_final)
+
+        return x
