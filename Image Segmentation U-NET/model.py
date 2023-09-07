dec80c1605771766a35d000674aab6fdea6a7546
Bhaskar Ghosh <bhaskar4647@gmail.com>
2023-09-07 15:20:55 -0500

    Added forward function for DoubleConv

--- Image Segmentation U-NET/model.py
+++ Image Segmentation U-NET/model.py
@@ -13,3 +13,9 @@ class DoubleConv(nn.Module):
             nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True),
         )
+
+    def forward(self, x):
+        return self.conv(x)
+    
+
+    
