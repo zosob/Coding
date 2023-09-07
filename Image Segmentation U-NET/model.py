2785c3e28effdd77811ea8c64a6f106b2b192e69
Bhaskar Ghosh <bhaskar4647@gmail.com>
2023-09-07 15:18:39 -0500

    Started Image Segmentation using U-Net with PyTorch

---
+++ Image Segmentation U-NET/model.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+import torchvision.transforms.functional as tf
+
+class DoubleConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(DoubleConv, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, 3, 1, 1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, 3, 1, 1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+        )
