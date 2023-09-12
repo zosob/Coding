6c10f264813b3555cf17460c8e82b452115d6bd9
Bhaskar Ghosh <bhaskar4647@gmail.com>
2023-09-12 10:52:08 -0500

    Completed forward functions

--- Image Segmentation U-NET/model.py
+++ Image Segmentation U-NET/model.py
@@ -43,4 +43,24 @@ class UNET(nn.Module):
         
         self.bottleneck = DoubleConv(features[-1], features[-1]*2)
         self.final_conv = nn.Conv2d(features[0], out_channels, kernel_size=1)
+
+    def forward(self, x):
+        skip_connections = []
+
+        for down in self.downs:
+            x = down(x)
+            skip_connections.append(x)
+            x = self.pool(x)
+
+        x = self.bottleneck(x)
+        skip_connections = skip_connections[::-1]
+
+        for idx in range(0, len(self.ups), 2):
+            x = self.ups[idx](x)
+            skip_connections = skip_connections[idx//2]
+            concat_skip = torch.cat((skip_connections, x), dim = 1)
+            x = self.ups[idx+1](concat_skip)
+        
+        return self.final_conv(x)
+                         
    
